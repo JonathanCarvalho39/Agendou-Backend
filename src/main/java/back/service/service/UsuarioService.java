231569ac16d4ff98c8150f13021ed0fe5c9edad3
luana <luana.leite@sptech.school>
2024-11-22 21:49:33 -0300

    finalizando ajustes nos services e controllers

--- src/main/java/back/service/service/UsuarioService.java
+++ src/main/java/back/service/service/UsuarioService.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
