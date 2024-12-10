b6c30c463fed14ee43f3f3344ad8010e00fb01e2
luana <luana.leite@sptech.school>
2024-12-09 23:56:34 -0300

    ajuste de cors

--- src/main/java/back/api/config/WebConfig.java
+++ src/main/java/back/api/config/WebConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPT IONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false)
+                .exposedHeaders("Authorization", "Content-Type")
+                .maxAge(3600L);
     }
 }
